1be0da59a3890cd93127da7e19f1f48b74e870ea
Tommie McAfee <tommie@couchbase.com>
2017-11-13 16:23:59 -0700

    Script to collect kubernetes cluster info
    usage: ./cb_k8s_support.sh
    
    Creates an archive of interesting cluster info in
    current working directory: 'cb-k8s-support.tgz'
    
    Change-Id: I8181ef9fa2c7e75b98411b4543e4c00ba1a5b16e
    Reviewed-on: http://review.couchbase.org/85486
    Reviewed-by: Mike Wiederhold <mike@couchbase.com>
    Tested-by: Tommie McAfee <tommie@couchbase.com>

---
+++ scripts/support/cb_k8s_support.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SUPPORT_PKG=cb-k8s-support
+OUTPUT_DIR=/tmp/$SUPPORT_PKG
+mkdir -p $OUTPUT_DIR
+
+# cluster debugging and diagnosis
+echo "collecting cluster info"
+kubectl cluster-info dump > $OUTPUT_DIR/cluster_info.logs
+kubectl describe nodes > $OUTPUT_DIR/describe_nodes.logs
+kubectl top node > $OUTPUT_DIR/top_nodes.logs   2>&1
+kubectl top pod > $OUTPUT_DIR/top_pods.logs   2>&1
+
+# info about pods, deployments, replicasets, and statefulsets
+echo "collecting info about cluster objects"
+kubectl describe --show-events --all-namespaces po > $OUTPUT_DIR/describe_pods.logs
+kubectl describe --show-events --all-namespaces deployment > $OUTPUT_DIR/describe_deployment.logs
+kubectl describe --show-events --all-namespaces rs > $OUTPUT_DIR/describe_rs.logs
+kubectl describe --show-events --all-namespaces statefulset > $OUTPUT_DIR/describe_statefulsets.logs
+
+# container logs
+echo "collecting container logs"
+for ns in $( kubectl get namespaces -o name | sed 's/.*\///' ); do
+  for pod in $( kubectl get -n $ns po -o name | sed 's/.*\///' ); do
+      echo "----------$ns/$pod----------"  >> $OUTPUT_DIR/containers.logs
+      kubectl describe -n $ns po/$pod  | grep -B1 "Container ID" | grep ":$" | sed 's/://' | xargs -I '{}' kubectl -n $ns logs $pod  -c '{}' >>  $OUTPUT_DIR/containers.logs
+  done
+done
+
+tar -czf $SUPPORT_PKG.tgz -C /tmp $SUPPORT_PKG
+rm -rf $OUTPUT_DIR
+echo "Done! `pwd`/$SUPPORT_PKG.tgz"
