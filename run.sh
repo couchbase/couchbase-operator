b87dee059d1b3f2e538c179798319e611fd5b358
Rita Zhang <rita.z.zhang@gmail.com>
2017-11-14 19:32:35 -0800

    Prep for PR

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-make container
-docker push ritak8s18reg.azurecr.io/couchbase-operator:v1.0.2
+#make container
+#docker push $REGISTRY/couchbase-operator:v1.0.2
 k delete -f example/couchbase-cluster.yaml
 k delete -f example/deployment.yaml
 k delete crd couchbaseclusters.couchbase.database.couchbase.com
