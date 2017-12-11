36a489d9179f3b5c27e4401e5a63d657c5bbd7c1
Tommie McAfee <tommie@couchbase.com>
2017-12-11 12:29:36 -0700

    K8S-34: collect service info and use subdirs
    Collects endpoint, service, and ingress info.
    Also divides the type of resources into subdirectories
    
    $> cluster		containers	resources	services
    
    Change-Id: Ibe27138c51e29990eb7668a297e049656100ddc3
    Reviewed-on: http://review.couchbase.org/86672
    Reviewed-by: Mike Wiederhold <mike@couchbase.com>
    Tested-by: Tommie McAfee <tommie@couchbase.com>

--- scripts/support/cb_k8s_support.sh
+++ scripts/support/cb_k8s_support.sh
@@ -5,19 +5,26 @@ OUTPUT_DIR=/tmp/$SUPPORT_PKG
 mkdir -p $OUTPUT_DIR
 
 # cluster debugging and diagnosis
+CLUSTER_LOG_DIR=$OUTPUT_DIR/cluster
+mkdir $CLUSTER_LOG_DIR
 echo "collecting cluster info"
-kubectl cluster-info dump > $OUTPUT_DIR/cluster_info.logs
-kubectl describe nodes > $OUTPUT_DIR/describe_nodes.logs
-kubectl top node > $OUTPUT_DIR/top_nodes.logs   2>&1
-kubectl top pod > $OUTPUT_DIR/top_pods.logs   2>&1
+kubectl cluster-info dump > $CLUSTER_LOG_DIR/cluster_info.logs
+kubectl describe nodes > $CLUSTER_LOG_DIR/describe_nodes.logs
+kubectl top node > $CLUSTER_LOG_DIR/top_nodes.logs   2>&1
+kubectl top pod > $CLUSTER_LOG_DIR/top_pods.logs   2>&1
 
 # info about pods, deployments, replicasets, and statefulsets
+RESOURCE_LOG_DIR=$OUTPUT_DIR/resources
+mkdir $RESOURCE_LOG_DIR
 echo "collecting info about cluster objects"
-kubectl describe --show-events --all-namespaces po > $OUTPUT_DIR/describe_pods.logs
-kubectl describe --show-events --all-namespaces deployment > $OUTPUT_DIR/describe_deployment.logs
-kubectl describe --show-events --all-namespaces rs > $OUTPUT_DIR/describe_rs.logs
-kubectl describe --show-events --all-namespaces statefulset > $OUTPUT_DIR/describe_statefulsets.logs
-kubectl describe --show-events --all-namespaces couchbasecluster > $OUTPUT_DIR/describe_couchbase.logs
+kubectl describe --show-events --all-namespaces po > $RESOURCE_LOG_DIR/pods.logs
+kubectl describe --show-events --all-namespaces deployment > $RESOURCE_LOG_DIR/deployment.logs
+kubectl describe --show-events --all-namespaces rs > $RESOURCE_LOG_DIR/repliaset.logs
+kubectl describe --show-events --all-namespaces statefulset > $RESOURCE_LOG_DIR/statefulsets.logs
+kubectl describe --show-events --all-namespaces couchbasecluster > $RESOURCE_LOG_DIR/couchbase.logs
+
+# list of all running resources, 'roles','limits'...
+kubectl get --all-namespaces all -o yaml > $RESOURCE_LOG_DIR/all_resources.logs
 
 # all container logs
 CONTAINER_LOG_DIR=$OUTPUT_DIR/containers
@@ -33,6 +40,15 @@ for ns in $( kubectl get namespaces -o name | sed 's/.*\///' ); do
       kubectl describe -n $ns po/$pod  | grep -B1 "Container ID" | grep ":$" | sed 's/://' | xargs -I '{}' kubectl -n $ns logs $pod --timestamps=true  -p -c '{}' >>  $pod_log_path/previous.logs  2>&1
   done
 done
+
+# services info
+SERVICE_LOG_DIR=$OUTPUT_DIR/services
+mkdir $SERVICE_LOG_DIR
+echo "collecting service logs"
+kubectl describe --all-namespaces --show-events=true  endpoints > $SERVICE_LOG_DIR/endpoints.log
+kubectl describe --all-namespaces --show-events=true  svc > $SERVICE_LOG_DIR/services.log
+kubectl describe --all-namespaces --show-events=true  ing > $SERVICE_LOG_DIR/ingress.log
+
 
 tar -czf $SUPPORT_PKG.tgz -C /tmp $SUPPORT_PKG
 rm -rf $OUTPUT_DIR
