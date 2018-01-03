b81058e0d417dd83b04e5435f1febe7e955367b3
Mike Wiederhold <mikewied06@gmail.com>
2018-01-02 16:07:46 -0800

    Add test documentation and extra verification to TestCreateBucketCluster
    Added a check to make sure the cluster is available and balanced to
    the end of the test case.
    
    Change-Id: I8c612b465d905b588218bc565e6516e20f60d6e1
    Reviewed-on: http://review.couchbase.org/87345
    Tested-by: Mike Wiederhold <mike@couchbase.com>
    Reviewed-by: Tommie McAfee <tommie@couchbase.com>

--- test/e2e/basic_test.go
+++ test/e2e/basic_test.go
@@ -44,13 +44,17 @@ func TestCreateCluster(t *testing.T) {
 	}
 }
 
-// creates cluster with single bucket
+// Tests creation of a 3 node cluster with 1 bucket
+// 1. Create a 3 node Couchbase cluster with 1 bucket
+// 2. Check the events to make sure the operator took the correct actions
+// 3. Verifies that the cluster is balanced and all data is available
 func TestCreateBucketCluster(t *testing.T) {
 	if os.Getenv(envParallelTest) == envParallelTestTrue {
 		t.Parallel()
 	}
 	f := framework.Global
-	testCouchbase, err := e2eutil.NewClusterBasic(t, f.CRClient, f.Namespace, f.DefaultSecret.Name, 3, true)
+	clusterSize := 3
+	testCouchbase, err := e2eutil.NewClusterBasic(t, f.CRClient, f.Namespace, f.DefaultSecret.Name, clusterSize, true)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -69,5 +73,10 @@ func TestCreateBucketCluster(t *testing.T) {
 	}
 	if !expectedEvents.Compare(events) {
 		t.Fatalf(e2eutil.EventListCompareFailedString(expectedEvents, events))
+	}
+
+	err = e2eutil.WaitClusterStatusHealthy(t, f.CRClient, testCouchbase.Name, f.Namespace, clusterSize, 18)
+	if err != nil {
+		t.Fatal(err.Error())
 	}
 }
