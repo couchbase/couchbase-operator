eee05f0af7e90b7ff49eae063474103aaa21ace5
Ashwin Govindarajulu <ashwintrojan+github@gmail.com>
2018-06-04 12:11:18 +0530

    K8S-351, Adding TLS testcases as part of Jenkins' job
     - Adding new function to append multiple result arrays
     - Calling analyze results individually on each testsuite
    
    Change-Id: I06bbb2c6299aa8d1b1af16e735c095f87622db25
    Reviewed-on: http://review.couchbase.org/94962
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Simon Murray <simon.murray@couchbase.com>

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -24,6 +24,9 @@ type TestResult struct {
 	result bool
 }
 
+// Variable to store the results globally
+var results = []TestResult{}
+
 // String variable to store the random suffix used for couchbase-server
 // name and tls certificates
 var RandomNameSuffix string
@@ -203,12 +206,14 @@ var testSuiteAll = TestSuite{
 	TestAntiAffinityOff,
 }
 
-var testSuiteTLS = TestSuite{
+var testSuiteTLSP0 = TestSuite{
 	// Basic tests
 	TestTlsCreateCluster,
 	TestTlsKillClusterNode,
 	TestTlsResizeCluster,
+}
 
+var testSuiteTLSP1 = TestSuite{
 	// Certificate remove tests
 	TestTlsRemoveOperatorCertificateAndAddBack,
 	TestTlsRemoveClusterCertificateAndAddBack,
@@ -234,6 +239,40 @@ func getTestFuncName(f TestFunc) string {
 	return nameParts[len(nameParts)-1]
 }
 
+// analyzeResults accepts a list of test results and displays success rates
+func analyzeResults(t *testing.T) {
+	t.Logf("Suite Test Results: \n")
+
+	failures := []string{}
+	for i, result := range results {
+		if result.result {
+			t.Logf("%d: %s...PASS", i+1, result.name)
+		}
+		if !result.result {
+			t.Logf("%d: %s...FAIL", i+1, result.name)
+			failures = append(failures, result.name)
+
+		}
+	}
+
+	pass := float64(len(results) - len(failures))
+	fail := float64(len(failures))
+	total := float64(len(results))
+	passRate := float64((pass / total) * 100.0)
+
+	if fail > 0 {
+		t.Logf("Failures: ")
+		for i, test := range failures {
+			t.Logf("%d: %s", i+1, test)
+		}
+	}
+
+	t.Logf("\n Pass: %f \n Fail: %f \n Pass Rate: %f", pass, fail, passRate)
+	if fail > 0 {
+		t.Fatalf("suite contains failures")
+	}
+}
+
 // runSuite takes a TestSuite and runs each test one at a time.  An optional list
 // of decorators may be applied to each test in the suite.  Decorators are
 // applied in order, so the first to be specified is the last to be run before
@@ -246,7 +285,6 @@ func getTestFuncName(f TestFunc) string {
 // is decorate immediately for a superset, or defer until we have an actual
 // set of tests
 func runSuite(t *testing.T, suite TestSuite, decorators ...TestDecorator) {
-	results := []TestResult{}
 	for _, test := range suite {
 		// Grab the test name before we possibly alter the test
 		// function pointer with decorators
@@ -261,52 +299,59 @@ func runSuite(t *testing.T, suite TestSuite, decorators ...TestDecorator) {
 		res := t.Run(name, test)
 		results = append(results, TestResult{name, res})
 	}
-
-	if analyzeResults(t, results) {
-		t.Fatalf("suite contains failures")
-	}
 }
 
 // Top-level test sets
 func TestSanity(t *testing.T) {
 	runSuite(t, testSuiteSanity)
+	analyzeResults(t)
 }
 
 func TestP0(t *testing.T) {
 	runSuite(t, testSuiteP0)
+	analyzeResults(t)
 }
 
 func TestP1(t *testing.T) {
 	runSuite(t, testSuiteP1)
+	analyzeResults(t)
 }
 
 func TestCRDValidation(t *testing.T) {
 	runSuite(t, testCRDValidation)
+	analyzeResults(t)
 }
 
 func TestAll(t *testing.T) {
 	runSuite(t, testSuiteAll)
+	analyzeResults(t)
 }
 
 func TestRSA(t *testing.T) {
-	runSuite(t, testSuiteTLS, rsaDecorator)
+	runSuite(t, testSuiteTLSP0, rsaDecorator)
+	runSuite(t, testSuiteTLSP1, rsaDecorator)
 	runSuite(t, testSuiteTLSCustCert)
+	analyzeResults(t)
 }
 
 func TestEllipticP224(t *testing.T) {
-	runSuite(t, testSuiteTLS, ellipticP224Decorator)
+	runSuite(t, testSuiteTLSP0, ellipticP224Decorator)
+	analyzeResults(t)
 }
 
 func TestEllipticP256(t *testing.T) {
-	runSuite(t, testSuiteTLS, ellipticP256Decorator)
+	runSuite(t, testSuiteTLSP0, ellipticP256Decorator)
+	analyzeResults(t)
 }
 
 func TestEllipticP384(t *testing.T) {
-	runSuite(t, testSuiteTLS, ellipticP384Decorator)
+	runSuite(t, testSuiteTLSP0, ellipticP384Decorator)
+	analyzeResults(t)
 }
 
 func TestEllipticP521(t *testing.T) {
-	runSuite(t, testSuiteTLS, ellipticP521Decorator)
+	runSuite(t, testSuiteTLSP0, ellipticP521Decorator)
+	analyzeResults(t)
 }
 
 func TestTLS(t *testing.T) {
@@ -319,37 +364,4 @@ func TestTLS(t *testing.T) {
 		TestEllipticP521,
 	}
 	runSuite(t, suite)
-}
-
-// analyzeResults accepts a list of test results and displays success rates
-func analyzeResults(t *testing.T, testResults []TestResult) bool {
-	t.Logf("Suite Test Results: \n")
-
-	failures := []string{}
-	for i, result := range testResults {
-		if result.result {
-			t.Logf("%d: %s...PASS", i+1, result.name)
-		}
-		if !result.result {
-			t.Logf("%d: %s...FAIL", i+1, result.name)
-			failures = append(failures, result.name)
-
-		}
-	}
-
-	pass := float64(len(testResults) - len(failures))
-	fail := float64(len(failures))
-	total := float64(len(testResults))
-	passRate := float64((pass / total) * 100.0)
-
-	if fail > 0 {
-		t.Logf("Failures: ")
-		for i, test := range failures {
-			t.Logf("%d: %s", i+1, test)
-		}
-	}
-
-	t.Logf("\n Pass: %f \n Fail: %f \n Pass Rate: %f", pass, fail, passRate)
-	containsFailure := fail > 0
-	return containsFailure
 }
