a48170a61dadd63ad6957f916e8ac90ec251c084
Ashwin Govindarajulu <ashwintrojan+github@gmail.com>
2018-06-28 23:03:18 +0530

    Adding code to create / remove node-port service
    Change-Id: I7010e3a80669e2e65bae8839621f3d625f614e4f
    Reviewed-on: http://review.couchbase.org/96291
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Korrigan Clark <korrigan.clark@couchbase.com>

--- test/e2e/e2eutil/port_forward.go
+++ test/e2e/e2eutil/port_forward.go
@@ -5,9 +5,12 @@ import (
 	"net/http"
 	"testing"
 
+	"k8s.io/api/core/v1"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/scheme"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/portforward"
 	"k8s.io/client-go/transport/spdy"
@@ -24,6 +27,15 @@ type PortForwarder struct {
 	errorChan chan error
 	out       *bytes.Buffer
 	errOut    *bytes.Buffer
+}
+
+// NodePort creates the service from pod with matching selectors to k8s cluster node
+type ServicePort v1.ServicePort
+type NodePortService struct {
+	ServiceName     string
+	ServicePortList []v1.ServicePort
+	Selectors       map[string]string
+	Namespace       string
 }
 
 // ForwardPorts accepts a Kubernetes configuration, pod and port and forwards
@@ -87,4 +99,20 @@ func (pf *PortForwarder) Close(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func (nodePort *NodePortService) CreateNodePortService(kubeClient kubernetes.Interface) error {
+	nodePortSpec := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: nodePort.ServiceName},
+		Spec: v1.ServiceSpec{
+			Ports:    nodePort.ServicePortList,
+			Selector: nodePort.Selectors,
+		},
+	}
+	_, err := kubeClient.CoreV1().Services(nodePort.Namespace).Create(nodePortSpec)
+	return err
+}
+
+func (nodePort *NodePortService) DeleteNodePortService(kubeClient kubernetes.Interface) error {
+	return kubeClient.CoreV1().Services(nodePort.Namespace).Delete(nodePort.ServiceName, &metav1.DeleteOptions{})
 }
