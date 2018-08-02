e35625b7f2e5e6a1065c0d0b03d9b64f2393b336
Mike Wiederhold <mikewied06@gmail.com>
2018-08-02 15:31:50 -0700

    Fix go vet issues
    Change-Id: I5b09f0028ead0c56ea0c251104acfbf4edfdac92
    Reviewed-on: http://review.couchbase.org/97754
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Mike Wiederhold <mike@couchbase.com>

--- test/e2e/e2eutil/wait_util.go
+++ test/e2e/e2eutil/wait_util.go
@@ -346,14 +346,12 @@ func waitPodsDeleted(kubecli kubernetes.Interface, namespace string, retries int
 
 // WaitUntilOperatorReady will wait until the first pod selected for the label name=couchbase-operator is ready.
 func WaitUntilOperatorReady(kubecli kubernetes.Interface, namespace, label string) error {
-	var podName string
 	err := retryutil.Retry(Context, time.Second, 180, func() (bool, error) {
 		podList, err := kubecli.CoreV1().Pods(namespace).List(metav1.ListOptions{LabelSelector: label})
 		if err != nil {
 			return false, err
 		}
 		if len(podList.Items) > 0 {
-			podName = podList.Items[0].Name
 			if k8sutil.IsPodReady(&podList.Items[0]) {
 				return true, nil
 			}
