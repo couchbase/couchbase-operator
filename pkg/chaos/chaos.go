f27397b5fee22dd408c20e99ecf8bffe13457e42
Tommie McAfee <tommie@couchbase.com>
2018-09-14 22:00:59 +0000

    Give Chaos module separate logger
    This helps when parsing through logs for activity generated by chaos.
    
    Change-Id: Ibe4f97c5a9f3fbf2a3031269c1e37390a94a496a
    Reviewed-on: http://review.couchbase.org/99646
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Simon Murray <simon.murray@couchbase.com>

--- pkg/chaos/chaos.go
+++ pkg/chaos/chaos.go
@@ -30,6 +30,7 @@ type CrashConfig struct {
 	KillProbability float64
 	KillMax         int
 	MinPods         int
+	logger          *logrus.Entry
 }
 
 // TODO: respect context in k8s operations.
@@ -44,22 +45,22 @@ func (m *Monkeys) CrushPods(ctx context.Context, c *CrashConfig) {
 	for {
 		err := limiter.Wait(ctx)
 		if err != nil { // user cancellation
-			logrus.Infof("crushPods is canceled for selector %v by the user: %v", ls, err)
+			c.logger.Infof("crushPods is canceled for selector %v by the user: %v", ls, err)
 			return
 		}
 
 		if p := rand.Float64(); p > c.KillProbability {
-			logrus.Infof("skip killing pod: probability: %v, got p: %v", c.KillProbability, p)
+			c.logger.Infof("skip killing pod: probability: %v, got p: %v", c.KillProbability, p)
 			continue
 		}
 
 		pods, err := m.kubecli.CoreV1().Pods(ns).List(metav1.ListOptions{LabelSelector: ls})
 		if err != nil {
-			logrus.Errorf("failed to list pods for selector %v: %v", ls, err)
+			c.logger.Errorf("failed to list pods for selector %v: %v", ls, err)
 			continue
 		}
 		if len(pods.Items) == 0 {
-			logrus.Infof("no pods to kill for selector %v", ls)
+			c.logger.Infof("no pods to kill for selector %v", ls)
 			continue
 		}
 
@@ -71,12 +72,12 @@ func (m *Monkeys) CrushPods(ctx context.Context, c *CrashConfig) {
 		if len(pods.Items)-max < c.MinPods {
 			max -= 1
 			if max == 0 {
-				logrus.Infof("skip killing pod: min pods required ", c.MinPods)
+				c.logger.Infof("skip killing pod: min pods required %d", c.MinPods)
 				continue
 			}
 		}
 
-		logrus.Infof("start to kill %d pods for selector %v", max, ls)
+		c.logger.Infof("start to kill %d pods for selector %v", max, ls)
 
 		tokills := make(map[string]struct{})
 		for len(tokills) < max {
@@ -86,10 +87,10 @@ func (m *Monkeys) CrushPods(ctx context.Context, c *CrashConfig) {
 		for tokill := range tokills {
 			err = m.kubecli.CoreV1().Pods(ns).Delete(tokill, metav1.NewDeleteOptions(0))
 			if err != nil {
-				logrus.Errorf("failed to kill pod %v: %v", tokill, err)
+				c.logger.Errorf("failed to kill pod %v: %v", tokill, err)
 				continue
 			}
-			logrus.Infof("killed pod %v for selector %v", tokill, ls)
+			c.logger.Infof("killed pod %v for selector %v", tokill, ls)
 		}
 	}
 }
@@ -97,10 +98,11 @@ func (m *Monkeys) CrushPods(ctx context.Context, c *CrashConfig) {
 func Start(ctx context.Context, kubecli kubernetes.Interface, ns string, chaosLevel int) {
 	m := NewMonkeys(kubecli)
 	ls := labels.SelectorFromSet(map[string]string{"app": "couchbase"})
+	logger := logrus.WithField("module", "chaos")
 
 	switch chaosLevel {
 	case 1:
-		logrus.Info("chaos level = 1: randomly kill one couchbase pod every 30 seconds at 50%")
+		logger.Info("chaos level = 1: randomly kill one couchbase pod every 30 seconds at 50%")
 		c := &CrashConfig{
 			Namespace: ns,
 			Selector:  ls,
@@ -109,13 +111,14 @@ func Start(ctx context.Context, kubecli kubernetes.Interface, ns string, chaosLe
 			KillProbability: 0.5,
 			KillMax:         2,
 			MinPods:         1,
+			logger:          logger,
 		}
 		go func() {
 			time.Sleep(30 * time.Second)
 			m.CrushPods(ctx, c)
 		}()
 	case 2:
-		logrus.Info("chaos level = 2: randomly kill at most two couchbase pod with at least 1 alive every 2 minutes at 50%")
+		logger.Info("chaos level = 2: randomly kill at most two couchbase pod with at least 1 alive every 2 minutes at 50%")
 		c := &CrashConfig{
 			Namespace: ns,
 			Selector:  ls,
@@ -124,13 +127,14 @@ func Start(ctx context.Context, kubecli kubernetes.Interface, ns string, chaosLe
 			KillProbability: 0.5,
 			KillMax:         2,
 			MinPods:         1,
+			logger:          logger,
 		}
 		go func() {
 			time.Sleep(300 * time.Second)
 			m.CrushPods(ctx, c)
 		}()
 	case 3:
-		logrus.Info("chaos level = 3: randomly kill at most two couchbase pod every 2 minutes at 50%")
+		logger.Info("chaos level = 3: randomly kill at most two couchbase pod every 2 minutes at 50%")
 		c := &CrashConfig{
 			Namespace: ns,
 			Selector:  ls,
@@ -139,13 +143,14 @@ func Start(ctx context.Context, kubecli kubernetes.Interface, ns string, chaosLe
 			KillProbability: 0.5,
 			KillMax:         2,
 			MinPods:         0,
+			logger:          logger,
 		}
 		go func() {
 			time.Sleep(300 * time.Second)
 			m.CrushPods(ctx, c)
 		}()
 	case 4:
-		logrus.Info("chaos level = 4: randomly kill at most five couchbase pods every 30 seconds at 50%")
+		logger.Info("chaos level = 4: randomly kill at most five couchbase pods every 30 seconds at 50%")
 		c := &CrashConfig{
 			Namespace: ns,
 			Selector:  ls,
@@ -154,6 +159,7 @@ func Start(ctx context.Context, kubecli kubernetes.Interface, ns string, chaosLe
 			KillProbability: 0.5,
 			KillMax:         5,
 			MinPods:         0,
+			logger:          logger,
 		}
 
 		go m.CrushPods(ctx, c)
