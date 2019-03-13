21f835ca575951aaef27660466f33d5597fd9c15
Simon Murray <spjmurray@yahoo.co.uk>
2019-03-13 15:55:03 +0000

    K8S-898: Fix PVC Waits
    We need to set an annotation to prevent waits on lazy bound PVCs.  No
    other way that a quick nasty hack until we can rewrite the CRD stuff
    which is frankly a mess.
    
    Change-Id: I8033102a4451ed0bc0f7ce28f3b1f690da180a16
    Reviewed-on: http://review.couchbase.org/106132
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Tommie McAfee <tommie@couchbase.com>

--- test/e2e/e2espec/crd.go
+++ test/e2e/e2espec/crd.go
@@ -10,6 +10,7 @@ import (
 	e2e_constants "github.com/couchbase/couchbase-operator/test/e2e/constants"
 
 	"k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -70,11 +71,16 @@ func SetCbImageVersion(cbImgVer string) {
 }
 
 var storageClassName string
+var isStorageClassLazyBound bool
 
 func SetStorageClassName(storageClassNameIn string) {
 	if storageClassNameIn = strings.TrimSpace(storageClassNameIn); storageClassNameIn != "" {
 		storageClassName = storageClassNameIn
 	}
+}
+
+func SetStorageClassLazy() {
+	isStorageClassLazyBound = true
 }
 
 func GetCouchbaseDockerImgName() string {
@@ -279,7 +285,8 @@ func NewSupportableClusterSpec(size int) api.ClusterSpec {
 		VolumeClaimTemplates: []v1.PersistentVolumeClaim{
 			{
 				ObjectMeta: metav1.ObjectMeta{
-					Name: "couchbase",
+					Name:        "couchbase",
+					Annotations: map[string]string{},
 				},
 				Spec: v1.PersistentVolumeClaimSpec{
 					StorageClassName: &storageClassName,
@@ -291,6 +298,11 @@ func NewSupportableClusterSpec(size int) api.ClusterSpec {
 				},
 			},
 		},
+	}
+
+	// HACK HACK HACK
+	if isStorageClassLazyBound {
+		spec.VolumeClaimTemplates[0].Annotations[constants.AnnotationVolumeBindingMode] = string(storagev1.VolumeBindingWaitForFirstConsumer)
 	}
 
 	// The defaults are too aggressive.  When killing a pod during a rebalance the operator
