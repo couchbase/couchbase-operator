80c18abba5c54f7a10979e215196c64fb7f91bde
roo.thorp <roo.thorp@couchbase.com>
2020-12-15 15:33:43 +0000

    K8S-1815: Auto-Provisioning using Terraform
    Autoprovision using Terraform to help with reliability and resilience,
    as well as making the configuration much more accessable and
    customisable, meaning tweaks in future are much easier.
    
    Change-Id: I044fca679da7f25651531ceb05878fba506e84be
    Reviewed-on: http://review.couchbase.org/c/couchbase-operator/+/142379
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>

---
+++ test/e2e/resources/provisioners/aks/aks.tf
@@ -0,0 +1,163 @@
+# Configure the Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription-id
+  client_id       = var.service-principal-id
+  client_secret   = var.service-principal-secret
+  tenant_id       = var.tenant-id
+  features {}
+}
+
+# Create resource group
+resource "azurerm_resource_group" "qe-auto" {
+  name     = "qe-auto"
+  location = "East US 2"
+}
+
+
+# Create VNet 1
+resource "azurerm_virtual_network" "qe-auto-vnet1" {
+  name                = "qe-auto-vnet1"
+  location            = "East US 2"
+  resource_group_name = azurerm_resource_group.qe-auto.name
+  address_space       = ["10.0.0.0/12"]
+}
+
+# Create Subnet for VNet 1
+resource "azurerm_subnet" "qe-auto-vnet1-subnet" {
+  name                  = "qe-auto-vnet1-subnet"
+  resource_group_name   = azurerm_resource_group.qe-auto.name
+  virtual_network_name  = azurerm_virtual_network.qe-auto-vnet1.name
+  address_prefixes      = ["10.8.0.0/16"]
+}
+
+# Create Cluster 1
+resource "azurerm_kubernetes_cluster" "qe-auto-cluster1" {
+  name                = "qe-auto-cluster1"
+  location            = "East US 2"
+  resource_group_name = azurerm_resource_group.qe-auto.name
+  dns_prefix          = "qe-auto-cluster1"
+  kubernetes_version  = var.kubernetes-version
+
+  default_node_pool {
+    name                = "nodepool1"
+    node_count          = 3
+    vm_size             = "Standard_D8s_v3"
+    availability_zones  = ["1", "2", "3"]
+    os_disk_size_gb     = 30
+    vnet_subnet_id      = azurerm_subnet.qe-auto-vnet1-subnet.id
+  }
+
+  network_profile {
+    network_plugin      = "azure"
+    dns_service_ip      = "10.0.0.10"
+    docker_bridge_cidr  = "172.17.0.1/16"
+    service_cidr        = "10.0.0.0/16"
+  }
+
+  addon_profile {
+    http_application_routing {
+      enabled = true
+    }
+  }
+
+  service_principal {
+    client_id     = var.service-principal-id
+    client_secret = var.service-principal-secret
+  }
+
+}
+
+# Do it all again for a 2nd cluster!
+
+# Create VNet 2
+resource "azurerm_virtual_network" "qe-auto-vnet2" {
+  name                = "qe-auto-vnet2"
+  location            = "West US 2"
+  resource_group_name = azurerm_resource_group.qe-auto.name
+  address_space       = ["10.16.0.0/12"]
+}
+
+# Create Subnet for VNet 2
+resource "azurerm_subnet" "qe-auto-vnet2-subnet" {
+  name                  = "qe-auto-vnet2-subnet"
+  resource_group_name   = azurerm_resource_group.qe-auto.name
+  virtual_network_name  = azurerm_virtual_network.qe-auto-vnet2.name
+  address_prefixes      = ["10.24.0.0/16"]
+}
+
+
+# Create Cluster 2
+resource "azurerm_kubernetes_cluster" "qe-auto-cluster2" {
+  name                = "qe-auto-cluster2"
+  location            = "West US 2"
+  resource_group_name = azurerm_resource_group.qe-auto.name
+  dns_prefix          = "qe-auto-cluster2"
+  kubernetes_version  = var.kubernetes-version
+
+  default_node_pool {
+    name                = "nodepool2"
+    node_count          = 3
+    vm_size             = "Standard_D8s_v3"
+    availability_zones  = ["1", "2", "3"]
+    os_disk_size_gb     = 30
+    vnet_subnet_id      = azurerm_subnet.qe-auto-vnet2-subnet.id
+  }
+
+  network_profile {
+    network_plugin      = "azure"
+    dns_service_ip      = "10.16.0.10"
+    docker_bridge_cidr  = "172.17.0.1/16"
+    service_cidr        = "10.16.0.0/16"
+  }
+
+  addon_profile {
+    http_application_routing {
+      enabled = true
+    }
+  }
+
+  service_principal {
+    client_id     = var.service-principal-id
+    client_secret = var.service-principal-secret
+  }
+
+}
+
+# Peer the VNets so they can chat
+
+resource "azurerm_virtual_network_peering" "qe-auto-peer-1to2" {
+  name = "qe-auto-peer-1to2"
+  resource_group_name       = azurerm_resource_group.qe-auto.name
+  virtual_network_name      = azurerm_virtual_network.qe-auto-vnet1.name
+  remote_virtual_network_id = azurerm_virtual_network.qe-auto-vnet2.id
+  allow_forwarded_traffic   = true
+}
+
+resource "azurerm_virtual_network_peering" "qe-auto-peer-2to1" {
+  name = "qe-auto-peer-2to1"
+  resource_group_name       = azurerm_resource_group.qe-auto.name
+  virtual_network_name      = azurerm_virtual_network.qe-auto-vnet2.name
+  remote_virtual_network_id = azurerm_virtual_network.qe-auto-vnet1.id
+  allow_forwarded_traffic   = true
+}
+
+# Output the kubeconfigs for the two clusters
+
+output "kubeconfig1" {
+  value = azurerm_kubernetes_cluster.qe-auto-cluster1.kube_config_raw
+  sensitive = true
+}
+
+output "kubeconfig2" {
+  value = azurerm_kubernetes_cluster.qe-auto-cluster2.kube_config_raw
+  sensitive = true
+}
---
+++ test/e2e/resources/provisioners/eks/eks.tf
@@ -0,0 +1,235 @@
+terraform {
+  required_providers {
+    kubernetes = {
+        version                = "~> 1.9"
+    }
+  }
+}
+
+provider "kubernetes" {
+  alias = "kube1"
+  host                   = data.aws_eks_cluster.cluster1.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster1.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster1.token
+  load_config_file       = false
+}
+
+provider "kubernetes" {
+  alias = "kube2"
+  host                   = data.aws_eks_cluster.cluster2.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster2.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster2.token
+  load_config_file       = false
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "east"
+}
+
+module "vpc1" {
+  source          = "terraform-aws-modules/vpc/aws"
+  name            = "qe-auto-vpc1"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  default_security_group_egress = [
+    {
+      description = "Egress allowing ALL"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  default_security_group_ingress = [
+    {
+      description = "Ingress allowing ALL"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+}
+
+resource "aws_security_group" "securitygroup1" {
+  name    = "qe-auto-securitygroup1"
+  vpc_id  = module.vpc1.vpc_id
+
+  ingress {
+    description = "Ingress allowing ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Egress allowing ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+module "vpc2" {
+  source          = "terraform-aws-modules/vpc/aws"
+  name            = "qe-auto-vpc2"
+  cidr            = "192.168.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+
+  default_security_group_egress = [
+    {
+      description = "Egress allowing ALL"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  default_security_group_ingress = [
+    {
+      description = "Ingress allowing ALL"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  providers = {
+    aws = aws.east
+  }
+}
+
+resource "aws_security_group" "securitygroup2" {
+  name      = "qe-auto-securitygroup2"
+  provider  = aws.east
+  vpc_id    = module.vpc2.vpc_id
+
+  ingress {
+    description = "Ingress allowing ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Egress allowing ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+data "aws_eks_cluster" "cluster1" {
+  name = module.cluster1.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster1" {
+  name = module.cluster1.cluster_id
+}
+
+module "cluster1" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "qe-auto-cluster1"
+  cluster_version = var.kubernetes-version
+  subnets         = module.vpc1.public_subnets
+  vpc_id          = module.vpc1.vpc_id
+
+  cluster_security_group_id             = aws_security_group.securitygroup1.id
+  worker_additional_security_group_ids  = [aws_security_group.securitygroup1.id]
+
+  worker_groups = [
+    {
+      instance_type         = "t3.medium"
+      asg_min_size          = 2
+      asg_desired_capacity  = 3
+      asg_max_size          = 4
+      root_volume_type      = "gp2"
+    }
+  ]
+  providers = {
+    kubernetes = kubernetes.kube1
+  }
+}
+
+data "aws_eks_cluster" "cluster2" {
+  name      = module.cluster2.cluster_id
+  provider  = aws.east
+}
+
+data "aws_eks_cluster_auth" "cluster2" {
+  name      = module.cluster2.cluster_id
+  provider  = aws.east
+}
+
+module "cluster2" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "qe-auto-cluster2"
+  cluster_version = var.kubernetes-version
+  subnets         = module.vpc2.public_subnets
+  vpc_id          = module.vpc2.vpc_id
+
+  cluster_security_group_id             = aws_security_group.securitygroup2.id
+  worker_additional_security_group_ids  = [aws_security_group.securitygroup2.id]
+
+  worker_groups = [
+    {
+      instance_type         = "t3.medium"
+      asg_min_size          = 2
+      asg_desired_capacity  = 3
+      asg_max_size          = 4
+      root_volume_type      = "gp2"
+    }
+  ]
+  providers = {
+    kubernetes  = kubernetes.kube2
+    aws         = aws.east
+  }
+}
+
+module "vpc_cross_region_peering" {
+  source = "github.com/grem11n/terraform-aws-vpc-peering"
+
+  count = var.peering ? 1 : 0
+
+  providers = {
+    aws.this = aws
+    aws.peer = aws.east
+  }
+
+  this_vpc_id             = module.vpc1.vpc_id
+  peer_vpc_id             = module.vpc2.vpc_id
+
+  auto_accept_peering     = true
+
+}
+
+output "kubeconfig1" {
+  value     = module.cluster1.kubeconfig
+  sensitive = true
+}
+
+output "kubeconfig2" {
+  value     = module.cluster2.kubeconfig
+  sensitive = true
+}
