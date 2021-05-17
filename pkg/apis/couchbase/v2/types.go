e4742ef4e42109f167315ebd03bc14bf25944064
Patrick Stephens <patrick.stephens@couchbase.com>
2021-05-17 09:32:40 +0100

    K8S-2190: Update to Fluent Bit 1.0.1 by default
    As 1.0.1 is available on RHCC now, update default to use it.
    There is no real difference between 1.0.0 and 1.0.1 other than licence files in a particular directory for Red Hat checks.
    
    Change-Id: I37479e9df4228d2e207aa5e3ae632d84d7d1e7b1
    Reviewed-on: http://review.couchbase.org/c/couchbase-operator/+/153644
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Simon Murray <simon.murray@couchbase.com>

--- pkg/apis/couchbase/v2/types.go
+++ pkg/apis/couchbase/v2/types.go
@@ -2641,7 +2641,7 @@ type LogShipperSidecarSpec struct {
 	// Image is the image to be used to deal with logging as a sidecar.
 	// No validation is carried out as this can be any arbitrary repo and tag.
 	// It will default to the latest supported version of Fluent Bit.
-	// +kubebuilder:default="couchbase/fluent-bit:1.0.0"
+	// +kubebuilder:default="couchbase/fluent-bit:1.0.1"
 	Image string `json:"image,omitempty"`
 
 	// ConfigurationMountPath is the location to mount the ConfigurationName Secret into the image.
