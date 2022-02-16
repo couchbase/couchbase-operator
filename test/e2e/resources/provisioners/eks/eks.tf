a6f5bb9f1f2b654b336b47ca11836aae9423f2eb
Roo Thorp <roo.thorp@couchbase.com>
2022-02-16 15:30:40 +0000

    K8S-2634: Tagging Terraform Two
    Terraform tags for 2.2.x branch
    
    Change-Id: I4b237b82b63dc9a33c79e67268d49bbc832e29f8
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/170891
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Prateek Kumar <prateekkumar276@gmail.com>

--- test/e2e/resources/provisioners/eks/eks.tf
+++ test/e2e/resources/provisioners/eks/eks.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     kubernetes = {
-        version                = "~> 1.9"
+      version = "~> 1.9"
     }
   }
 }
 
 provider "kubernetes" {
-  alias = "kube1"
+  alias                  = "kube1"
   host                   = data.aws_eks_cluster.cluster1.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster1.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster1.token
@@ -15,7 +15,7 @@ provider "kubernetes" {
 }
 
 provider "kubernetes" {
-  alias = "kube2"
+  alias                  = "kube2"
   host                   = var.remote ? data.aws_eks_cluster.cluster2[0].endpoint : null
   cluster_ca_certificate = var.remote ? base64decode(data.aws_eks_cluster.cluster2[0].certificate_authority.0.data) : null
   token                  = var.remote ? data.aws_eks_cluster_auth.cluster2[0].token : null
@@ -24,19 +24,30 @@ provider "kubernetes" {
 
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      Owner = "cao"
+    }
+  }
 }
 
 provider "aws" {
   region = "us-east-2"
-  alias = "east"
+  alias  = "east"
+  default_tags {
+    tags = {
+      Owner = "cao"
+    }
+  }
 }
 
 module "vpc1" {
-  source          = "terraform-aws-modules/vpc/aws"
-  name            = "${var.name}-vpc1"
-  cidr            = "10.0.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  source         = "terraform-aws-modules/vpc/aws"
+  version        = "~>3.12.0"
+  name           = "${var.name}-vpc1"
+  cidr           = "10.0.0.0/16"
+  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
   default_security_group_egress = [
     {
@@ -63,8 +74,8 @@ module "vpc1" {
 }
 
 resource "aws_security_group" "securitygroup1" {
-  name    = "${var.name}-securitygroup1"
-  vpc_id  = module.vpc1.vpc_id
+  name   = "${var.name}-securitygroup1"
+  vpc_id = module.vpc1.vpc_id
 
   ingress {
     description = "Ingress allowing ALL"
@@ -86,11 +97,12 @@ resource "aws_security_group" "securitygroup1" {
 module "vpc2" {
   count = var.remote ? 1 : 0
 
-  source          = "terraform-aws-modules/vpc/aws"
-  name            = "${var.name}-vpc2"
-  cidr            = "192.168.0.0/16"
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  public_subnets  = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+  source         = "terraform-aws-modules/vpc/aws"
+  version        = "~>3.12.0"
+  name           = "${var.name}-vpc2"
+  cidr           = "192.168.0.0/16"
+  azs            = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
 
   default_security_group_egress = [
     {
@@ -118,14 +130,15 @@ module "vpc2" {
   providers = {
     aws = aws.east
   }
+
 }
 
 resource "aws_security_group" "securitygroup2" {
   count = var.remote ? 1 : 0
 
-  name      = "${var.name}-securitygroup2"
-  provider  = aws.east
-  vpc_id    = module.vpc2[0].vpc_id
+  name     = "${var.name}-securitygroup2"
+  provider = aws.east
+  vpc_id   = module.vpc2[0].vpc_id
 
   ingress {
     description = "Ingress allowing ALL"
@@ -145,29 +158,36 @@ resource "aws_security_group" "securitygroup2" {
 }
 
 module "cluster1" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 17.24.0"
-  cluster_name    = "${var.name}-1"
-  cluster_version = var.kubernetes-version
-  subnets         = module.vpc1.public_subnets
-  vpc_id          = module.vpc1.vpc_id
+  source                   = "terraform-aws-modules/eks/aws"
+  version                  = "~> 17.24.0"
+  cluster_name             = "${var.name}-1"
+  cluster_version          = var.kubernetes-version
+  subnets                  = module.vpc1.public_subnets
+  vpc_id                   = module.vpc1.vpc_id
   wait_for_cluster_timeout = 600
 
-  cluster_security_group_id             = aws_security_group.securitygroup1.id
-  worker_additional_security_group_ids  = [aws_security_group.securitygroup1.id]
+  cluster_security_group_id            = aws_security_group.securitygroup1.id
+  worker_additional_security_group_ids = [aws_security_group.securitygroup1.id]
 
   worker_groups = [
     {
-      instance_type         = "m5.xlarge"
-      asg_min_size          = 2
-      asg_desired_capacity  = 3
-      asg_max_size          = 4
-      root_volume_type      = "gp2"
+      instance_type        = "m5.xlarge"
+      asg_min_size         = 2
+      asg_desired_capacity = 3
+      asg_max_size         = 4
+      root_volume_type     = "gp2"
+
+      tags = [{
+        key                 = "Owner"
+        value               = "cao"
+        propagate_at_launch = true
+      }]
     }
   ]
   providers = {
     kubernetes = kubernetes.kube1
   }
+
 }
 
 data "aws_eks_cluster" "cluster1" {
@@ -181,48 +201,55 @@ data "aws_eks_cluster_auth" "cluster1" {
 module "cluster2" {
   count = var.remote ? 1 : 0
 
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 17.24.0"
-  cluster_name    = "${var.name}-2"
-  cluster_version = var.kubernetes-version
-  subnets         = module.vpc2[0].public_subnets
-  vpc_id          = module.vpc2[0].vpc_id
+  source                   = "terraform-aws-modules/eks/aws"
+  version                  = "~> 17.24.0"
+  cluster_name             = "${var.name}-2"
+  cluster_version          = var.kubernetes-version
+  subnets                  = module.vpc2[0].public_subnets
+  vpc_id                   = module.vpc2[0].vpc_id
   wait_for_cluster_timeout = 600
 
-  cluster_security_group_id             = aws_security_group.securitygroup2[0].id
-  worker_additional_security_group_ids  = [aws_security_group.securitygroup2[0].id]
+  cluster_security_group_id            = aws_security_group.securitygroup2[0].id
+  worker_additional_security_group_ids = [aws_security_group.securitygroup2[0].id]
 
   worker_groups = [
     {
-      instance_type         = "m5.xlarge"
-      asg_min_size          = 2
-      asg_desired_capacity  = 3
-      asg_max_size          = 4
-      root_volume_type      = "gp2"
+      instance_type        = "m5.xlarge"
+      asg_min_size         = 2
+      asg_desired_capacity = 3
+      asg_max_size         = 4
+      root_volume_type     = "gp2"
+
+      tags = [{
+        key                 = "Owner"
+        value               = "cao"
+        propagate_at_launch = true
+      }]
     }
   ]
   providers = {
-    kubernetes  = kubernetes.kube2
-    aws         = aws.east
+    kubernetes = kubernetes.kube2
+    aws        = aws.east
   }
+
 }
 
 data "aws_eks_cluster" "cluster2" {
-  count = var.remote ? 1 : 0
-  name      = module.cluster2[0].cluster_id
-  provider  = aws.east
+  count    = var.remote ? 1 : 0
+  name     = module.cluster2[0].cluster_id
+  provider = aws.east
 }
 
 data "aws_eks_cluster_auth" "cluster2" {
-  count = var.remote ? 1 : 0
-  name      = module.cluster2[0].cluster_id
-  provider  = aws.east
+  count    = var.remote ? 1 : 0
+  name     = module.cluster2[0].cluster_id
+  provider = aws.east
 }
 
 resource "aws_vpc_peering_connection" "peering" {
   count = var.remote ? 1 : 0
 
-  vpc_id = module.vpc1.vpc_id
+  vpc_id      = module.vpc1.vpc_id
   peer_vpc_id = module.vpc2[0].vpc_id
   peer_region = "us-east-1"
   auto_accept = false
@@ -231,27 +258,27 @@ resource "aws_vpc_peering_connection" "peering" {
 resource "aws_vpc_peering_connection_accepter" "peering-accept" {
   count = var.remote ? 1 : 0
 
-  provider = aws.east
+  provider                  = aws.east
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-  auto_accept = true
+  auto_accept               = true
 }
 
 
 resource "aws_route" "peering-route-1" {
   count = var.remote ? 1 : 0
 
-  route_table_id = module.vpc1.public_route_table_ids[0]
-  destination_cidr_block = "192.168.0.0/16"
+  route_table_id            = module.vpc1.public_route_table_ids[0]
+  destination_cidr_block    = "192.168.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
 
 resource "aws_route" "peering-route-2" {
   count = var.remote ? 1 : 0
 
-  route_table_id = module.vpc2[0].public_route_table_ids[0]
-  destination_cidr_block = "10.0.0.0/16"
+  route_table_id            = module.vpc2[0].public_route_table_ids[0]
+  destination_cidr_block    = "10.0.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-  provider  = aws.east
+  provider                  = aws.east
 }
 
 output "kubeconfig1" {
