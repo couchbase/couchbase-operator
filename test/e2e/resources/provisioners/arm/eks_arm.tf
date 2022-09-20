dfcf959b35b18a0cb27230a834c6e6720cf6bd3b
Gilad Kalchheim <gilad.kalchheim@couchbase.com>
2022-09-20 12:55:45 +0100

    Initial commit for ARM support
    Change-Id: Id9c5e2c7ee581599c4074cdf9baf12f2cb3cb0be
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/180190
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Mike <mike.stanton@couchbase.com>

---
+++ test/e2e/resources/provisioners/arm/eks_arm.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      version = "~> 2.11"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+  default_tags {
+    tags = {
+      Owner = "cao"
+    }
+  }
+}
+
+module "vpc" {
+  source         = "terraform-aws-modules/vpc/aws"
+  version        = "~>3.12.0"
+  name           = "${var.name}-vpc"
+  cidr           = "10.0.0.0/16"
+  azs            = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  default_security_group_egress = [
+    {
+      description = "Egress allowing ALL"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  default_security_group_ingress = [
+    {
+      description = "Ingress allowing ALL"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+}
+
+resource "aws_security_group" "securitygroup" {
+  name   = "${var.name}-securitygroup"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "Ingress allowing ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Egress allowing ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+module "cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~>18.26.0"
+  cluster_name    = var.name
+  cluster_version = var.kubernetes-version
+  subnet_ids      = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  cluster_security_group_id = aws_security_group.securitygroup.id
+
+  eks_managed_node_groups = {
+    default_node_group = {
+      name                   = "cao-24x-pipeline"
+      ami_type               = "AL2_ARM_64"
+      id                     = data.aws_ami.arm-ami.id
+      instance_types         = ["m6g.xlarge"]
+      min_size               = 11
+      desired_size           = 12
+      max_size               = 13
+      vpc_security_group_ids = [aws_security_group.securitygroup.id]
+    }
+  }
+}
+
+data "aws_ami" "arm-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2*arm64-gp2"]
+  }
+}
---
+++ test/e2e/resources/provisioners/arm/variables.tf
@@ -0,0 +1,12 @@
+variable "kubernetes-version" {
+  type    = string
+  default = "1.17"
+}
+
+variable "name" {
+  type = string
+}
+
+variable "remote" {
+  default = false
+}
