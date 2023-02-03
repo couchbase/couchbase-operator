d865efe4e67a106367a27084619954413c31b249
Gilad Kalchheim <gilad.kalchheim@couchbase.com>
2023-02-03 10:21:19 +0000

    K8S-2961: Add EBS CSI driver to the EKS cluster, to support Kubernetes versions 1.23 and above.
    Change-Id: If5b0f6878da129a501aa184cd6c3e73d20dc2f76
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/186108
    Reviewed-by: Mike <mike.stanton@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- test/e2e/resources/provisioners/eks/eks.tf
+++ test/e2e/resources/provisioners/eks/eks.tf
@@ -89,3 +89,14 @@ module "cluster" {
     }
   }
 }
+# Required for Kubernetes 1.23 and above:
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
+module "kubernetes_addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons"
+  eks_cluster_id = module.cluster.cluster_id
+  enable_amazon_eks_aws_ebs_csi_driver = true
+  amazon_eks_aws_ebs_csi_driver_config = {
+    addon_name               = "aws-ebs-csi-driver"
+    addon_version            = "v1.5.2-eksbuild.1"
+  }
+ }
