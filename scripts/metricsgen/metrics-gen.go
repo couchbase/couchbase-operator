d000d60bc9c4aae0d4ba534d02cb52b8c303fb3b
Yusuf Ramzan <yusuf.ramzan@couchbase.com>
2023-07-19 14:47:35 +0100

    K8S-1592 Added script to auto-gen prometheus metrics doc
    Go script to automate the generation of prometheus metrics
    information. Script uses comments from above each metric to
    generate the doc.
    
    Change-Id: Ic1569c1e1e62e66e9d06f1b1d32a5d8d2a173643
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/194068
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Malarky <malarky@couchbase.com>
    Reviewed-by: Abhishek Bose <abhi.bose@couchbase.com>

---
+++ scripts/metricsgen/metrics-gen.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"strings"
+)
+
+type metricMetadata struct {
+	Type      string   `json:"type"`
+	Help      string   `json:"help"`
+	Unit      string   `json:"unit,omitempty"`
+	Added     string   `json:"added"`
+	Stability string   `json:"stability"`
+	Labels    []string `json:"labels"`
+}
+
+func main() {
+	fset := token.NewFileSet()
+	d, err := parser.ParseFile(fset, "pkg/metrics/metrics.go", nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	var metrics = map[string]metricMetadata{}
+	for _, c := range d.Comments {
+		var metricMetadata = new(metricMetadata)
+		var metricName string
+		for _, c1 := range c.List {
+			if strings.Contains(c1.Text, "name:") {
+				metricName = strings.TrimLeft(c1.Text, "// name: ")
+			}
+			if strings.Contains(c1.Text, "type:") {
+				metricMetadata.Type = strings.TrimLeft(c1.Text, "// type: ")
+			}
+			if strings.Contains(c1.Text, "help:") {
+				metricMetadata.Help = strings.TrimLeft(c1.Text, "// help: ")
+			}
+			if strings.Contains(c1.Text, "unit:") {
+				metricMetadata.Unit = strings.TrimLeft(c1.Text, "// unit: ")
+			}
+			if strings.Contains(c1.Text, "added:") {
+				metricMetadata.Added = strings.TrimLeft(c1.Text, "// added: ")
+			}
+			if strings.Contains(c1.Text, "stability:") {
+				metricMetadata.Stability = strings.TrimLeft(c1.Text, "// stability: ")
+			}
+			if strings.Contains(c1.Text, "labels:") {
+				labels := strings.TrimLeft(c1.Text, "// labels: ")
+				labels = strings.ReplaceAll(labels, " ", "")
+				s := strings.Split(labels, ",")
+				metricMetadata.Labels = s
+			}
+		}
+		metrics[metricName] = *metricMetadata
+	}
+
+	json, err := json.MarshalIndent(metrics, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+
+	error := ioutil.WriteFile("docs/user/modules/ROOT/attachments/cao_metrics_metadata.json", json, 0644)
+
+	if error != nil {
+		panic(error)
+	}
+}
