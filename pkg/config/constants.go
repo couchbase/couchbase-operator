5b5c4ab7bc56478f186f9b2fed0a81595d8edbdd
usamah jassat <usamah.jassat@couchbase.com>
2024-01-18 15:40:17 +0000

    K8S-3324: Fix supported Kubernetes versions
    Change-Id: Ie98f3399620e7609e2256e1c18cf6da0fcb01fe5
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/203944
    Reviewed-by: Justin Ashworth <justin.ashworth@couchbase.com>
    Tested-by: Justin Ashworth <justin.ashworth@couchbase.com>
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/209365
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <usamah.jassat@couchbase.com>

--- pkg/config/constants.go
+++ pkg/config/constants.go
@@ -5,10 +5,17 @@ package config
 
 import (
 	"github.com/couchbase/couchbase-operator/pkg/version"
+	k8sversion "k8s.io/apimachinery/pkg/version"
 )
 
 var (
 	admissionImageDefault = "couchbase/admission-controller:" + version.WithRevision()
 	operatorImageDefault  = "couchbase/operator:" + version.WithRevision()
 	caoBinaryName         = "cao"
+
+	// Note: these should be updated every release.
+
+	technicalLowerBound = &k8sversion.Info{Major: "1", Minor: "26", GitVersion: "v1.26.0"}
+	supportedLowerBound = &k8sversion.Info{Major: "1", Minor: "26", GitVersion: "v1.26.0"}
+	supportedUpperBound = &k8sversion.Info{Major: "1", Minor: "29", GitVersion: "v1.29.0"}
 )
