f944c31b9684a8def2943d00ca5674cc6e2516c6
usamah jassat <usamah.jassat@couchbase.com>
2024-01-19 10:50:15 +0000

    K8S-3324: Fixed supported versions
    Change-Id: I0791c47d09b08c36de3dfea6e94f7cbae3b54fad
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/209389
    Reviewed-by: <usamah.jassat@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- pkg/config/version_constants.go
+++ pkg/config/version_constants.go
@@ -4,7 +4,7 @@ import "k8s.io/apimachinery/pkg/version"
 
 var (
 	// Note: these should be updated every release.
-	technicalLowerBound = &version.Info{Major: "1", Minor: "26", GitVersion: "v1.26.0"}
-	supportedLowerBound = &version.Info{Major: "1", Minor: "26", GitVersion: "v1.26.0"}
+	technicalLowerBound = &version.Info{Major: "1", Minor: "24", GitVersion: "v1.24.0"}
+	supportedLowerBound = &version.Info{Major: "1", Minor: "24", GitVersion: "v1.24.0"}
 	supportedUpperBound = &version.Info{Major: "1", Minor: "29", GitVersion: "v1.29.0"}
 )
