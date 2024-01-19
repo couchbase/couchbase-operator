9e122e2a1c25fed6ae7d95d6524719e48065f313
usamah jassat <usamah.jassat@couchbase.com>
2024-01-19 10:50:15 +0000

    K8S-3324: Separate out version constants
    Change-Id: Ib1dd7365e650713f9816f61bf191db66b9f872b5
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/209371
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <usamah.jassat@couchbase.com>

---
+++ pkg/config/version_constants.go
@@ -0,0 +1,10 @@
+package config
+
+import "k8s.io/apimachinery/pkg/version"
+
+var (
+	// Note: these should be updated every release.
+	technicalLowerBound = &version.Info{Major: "1", Minor: "26", GitVersion: "v1.26.0"}
+	supportedLowerBound = &version.Info{Major: "1", Minor: "26", GitVersion: "v1.26.0"}
+	supportedUpperBound = &version.Info{Major: "1", Minor: "29", GitVersion: "v1.29.0"}
+)
