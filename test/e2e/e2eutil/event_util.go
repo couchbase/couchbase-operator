93d0323955aa18de7335b0bc3e522e0dadec0534
Yusuf Ramzan <yusuf.ramzan@couchbase.com>
2024-01-29 14:35:32 +0000

    K8S-3342 Fixed TestPersistentVolumeKillAllPodsAndOperator
    Change-Id: I16d06699ce18b78a2344c8d711c26c1d4fc44266
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/204583
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <usamah.jassat@couchbase.com>

--- test/e2e/e2eutil/event_util.go
+++ test/e2e/e2eutil/event_util.go
@@ -358,7 +358,7 @@ func PodDownWithPVCRecoverySequence(clusterSize, victims int) eventschema.Valida
 	events := eventschema.Sequence{}
 
 	if victims == clusterSize {
-		events.Validators = append(events.Validators, eventschema.Event{Reason: k8sutil.EventReasonMemberRecovered})
+		events.Validators = append(events.Validators, eventschema.Optional{Validator: eventschema.Event{Reason: k8sutil.EventReasonMemberRecovered}})
 		victims--
 	}
 
