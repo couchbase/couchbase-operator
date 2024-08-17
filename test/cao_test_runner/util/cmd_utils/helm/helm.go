2823195a6a4600bb918eb3f3e90f8ec1a07da47f
Raghav S K <raghav.sk@couchbase.com>
2024-08-17 23:55:39 +0530

    Deleting helm utils and adding it into the CmdUtils interface
    Change-Id: Ic53867d18e3b566a8b53ded19389bdeea3a720f0
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/214655
    Tested-by: Raghav S K <raghav.sk@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>

---
+++ test/cao_test_runner/util/cmd_utils/helm/helm.go
@@ -0,0 +1,76 @@
+package helm
+
+import (
+	"strings"
+
+	cmdutils "github.com/couchbase/couchbase-operator/test/cao_test_runner/util/cmd_utils"
+)
+
+const (
+	NumOfArgs = 12
+)
+
+type HelmCmd struct {
+	cmdutils.Cmd
+}
+
+// WithFlag adds a flag to the helm command. Flags are appended just after "helm" like: "helm --flag command".
+func (h *HelmCmd) WithFlag(name string, value string) *HelmCmd {
+	if h.Flags == nil {
+		h.Flags = make(map[string]string)
+	}
+
+	h.Flags[name] = value
+
+	return h
+}
+
+func (h *HelmCmd) InNamespace(namespace string) *HelmCmd {
+	return h.WithFlag("namespace", namespace)
+}
+
+func (h *HelmCmd) InContext(ctxt string) *HelmCmd {
+	return h.WithFlag("kube-context", ctxt)
+}
+
+func (h *HelmCmd) Set(args ...string) *HelmCmd {
+	value := strings.Join(args, " ")
+	h.WithFlag("set", value)
+
+	return h
+}
+
+func (h *HelmCmd) Edit(args ...string) *HelmCmd {
+	value := strings.Join(args, " ")
+	h.WithFlag("edit", value)
+
+	return h
+}
+
+// =============================================
+// =============== Helm Commands ===============
+// =============================================
+
+func List(args ...string) *HelmCmd {
+	return &HelmCmd{cmdutils.Cmd{RootCommand: "helm", Command: "list", Args: args}}
+}
+
+func Repo(args ...string) *HelmCmd {
+	return &HelmCmd{cmdutils.Cmd{RootCommand: "helm", Command: "repo", Args: args}}
+}
+
+func Pull(args ...string) *HelmCmd {
+	return &HelmCmd{cmdutils.Cmd{RootCommand: "helm", Command: "pull", Args: args}}
+}
+
+func Install(args ...string) *HelmCmd {
+	return &HelmCmd{cmdutils.Cmd{RootCommand: "helm", Command: "install", Args: args}}
+}
+
+func Upgrade(args ...string) *HelmCmd {
+	return &HelmCmd{cmdutils.Cmd{RootCommand: "helm", Command: "upgrade", Args: args}}
+}
+
+func Uninstall(args ...string) *HelmCmd {
+	return &HelmCmd{cmdutils.Cmd{RootCommand: "helm", Command: "uninstall", Args: args}}
+}
