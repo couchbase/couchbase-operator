261e1048279a4f878bae274201a494270941312c
Raghav S K <raghav.sk@couchbase.com>
2024-08-25 23:40:24 +0530

    Adding azure sdk in managed k8s services
    Change-Id: If82017690acfe641b82209d00a01cf7059b582e9
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/214971
    Tested-by: Raghav S K <raghav.sk@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>

--- test/cao_test_runner/go.mod
+++ test/cao_test_runner/go.mod
@@ -3,6 +3,11 @@ module github.com/couchbase/couchbase-operator/test/cao_test_runner
 go 1.22.5
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.14.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.7.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork v1.1.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.2.0
 	github.com/aws/aws-sdk-go v1.55.5
 	github.com/ghodss/yaml v1.0.0
 	github.com/goccy/go-yaml v1.12.0
@@ -14,18 +19,23 @@ require (
 )
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.10.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.2 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fatih/color v1.14.1 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
@@ -33,6 +43,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -43,11 +54,12 @@ require (
 	github.com/x448/float16 v0.8.4 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/term v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
--- test/cao_test_runner/managedk8sservices/aks.go
+++ test/cao_test_runner/managedk8sservices/aks.go
@@ -1,8 +1,21 @@
 package managedk8sservices
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"sync"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
+
+var (
+	ErrInvalidOSSKUType = errors.New("invalid os sku type")
+	ErrInvalidOSType    = errors.New("invalid os type")
 )
 
 type AKSSessionStore struct {
@@ -11,16 +24,57 @@ type AKSSessionStore struct {
 }
 
 type AKSSession struct {
-	Cred        *ManagedServiceCredentials
-	Region      string
-	ClusterName string
+	ResourceGroupClient   *armresources.ResourceGroupsClient
+	VirtualNetworksClient *armnetwork.VirtualNetworksClient
+	SubnetsClient         *armnetwork.SubnetsClient
+	AKSClient             *armcontainerservice.ManagedClustersClient
+	NodePoolClient        *armcontainerservice.AgentPoolsClient
+	Cred                  *ManagedServiceCredentials
+	Region                string
+	ClusterName           string
 }
 
 func NewAKSSession(managedSvcCred *ManagedServiceCredentials) (*AKSSession, error) {
+	cred, err := azidentity.NewClientSecretCredential(managedSvcCred.AKSCredentials.aksTenantID,
+		managedSvcCred.AKSCredentials.aksServicePrincipalID, managedSvcCred.AKSCredentials.aksServicePrincipalSecret, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create credentials: %w", err)
+	}
+
+	resourceGroupClient, err := armresources.NewResourceGroupsClient(managedSvcCred.AKSCredentials.aksSubscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create resource group client: %w", err)
+	}
+
+	vnetClient, err := armnetwork.NewVirtualNetworksClient(managedSvcCred.AKSCredentials.aksSubscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create virtual network client: %w", err)
+	}
+
+	subnetClient, err := armnetwork.NewSubnetsClient(managedSvcCred.AKSCredentials.aksSubscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create subnet client: %w", err)
+	}
+
+	aksClient, err := armcontainerservice.NewManagedClustersClient(managedSvcCred.AKSCredentials.aksSubscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create AKS client: %w", err)
+	}
+
+	nodePoolClient, err := armcontainerservice.NewAgentPoolsClient(managedSvcCred.AKSCredentials.aksSubscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create node pool client: %w", err)
+	}
+
 	return &AKSSession{
-		Cred:        managedSvcCred,
-		Region:      managedSvcCred.AKSCredentials.aksRegion,
-		ClusterName: managedSvcCred.ClusterName,
+		ResourceGroupClient:   resourceGroupClient,
+		VirtualNetworksClient: vnetClient,
+		SubnetsClient:         subnetClient,
+		AKSClient:             aksClient,
+		NodePoolClient:        nodePoolClient,
+		Cred:                  managedSvcCred,
+		Region:                managedSvcCred.AKSCredentials.aksRegion,
+		ClusterName:           managedSvcCred.ClusterName,
 	}, nil
 }
 
@@ -35,6 +89,24 @@ func NewAKSSessionStore() *AKSSessionStore {
 	return &AKSSessionStore{
 		AKSSessions: make(map[string]*AKSSession),
 		lock:        sync.Mutex{},
+	}
+}
+
+func ValidateOSSKUType(osSKU *armcontainerservice.OSSKU) error {
+	switch *osSKU {
+	case armcontainerservice.OSSKUCBLMariner, armcontainerservice.OSSKUUbuntu, "AzureLinux":
+		return nil
+	default:
+		return fmt.Errorf("invalid os sku type: %w", ErrInvalidOSSKUType)
+	}
+}
+
+func ValidateOSType(osType *armcontainerservice.OSType) error {
+	switch *osType {
+	case armcontainerservice.OSTypeLinux, armcontainerservice.OSTypeWindows:
+		return nil
+	default:
+		return fmt.Errorf("invalid os type: %w", ErrInvalidOSType)
 	}
 }
 
@@ -83,3 +155,194 @@ func (ass *AKSSessionStore) GetInstancesByK8sNodeName(managedSvcCred *ManagedSer
 // ================================================
 // ====== Methods implemented by AKSSession ======
 // ================================================
+
+func (aksSession *AKSSession) ListClusterUserCredentials(ctx *context.Context, resourceGroupName string) (*armcontainerservice.ManagedClustersClientListClusterUserCredentialsResponse, error) {
+	resp, err := aksSession.AKSClient.ListClusterUserCredentials(*ctx, resourceGroupName, aksSession.ClusterName, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get cluster credentials: %w", err)
+	}
+
+	return &resp, nil
+}
+
+func (aksSession *AKSSession) GetResourceGroup(ctx *context.Context, resourceGroupName string) (*armresources.ResourceGroupsClientGetResponse, error) {
+	resp, err := aksSession.ResourceGroupClient.Get(*ctx, resourceGroupName, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get resource group: %w", err)
+	}
+
+	return &resp, nil
+}
+
+func (aksSession *AKSSession) GetCluster(ctx *context.Context, resourceGroupName string) (*armcontainerservice.ManagedClustersClientGetResponse, error) {
+	cluster, err := aksSession.AKSClient.Get(*ctx, resourceGroupName, aksSession.ClusterName, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Kubernetes cluster: %w", err)
+	}
+
+	return &cluster, nil
+}
+
+func (aksSession *AKSSession) GetSubnet(ctx *context.Context, resourceGroupName, virtualNetworkName, subnetName string) (*armnetwork.SubnetsClientGetResponse, error) {
+	subnet, err := aksSession.SubnetsClient.Get(*ctx, resourceGroupName, virtualNetworkName, subnetName, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get subnet: %w", err)
+	}
+
+	return &subnet, nil
+}
+
+func (aksSession *AKSSession) CreateResourceGroup(ctx *context.Context, resourceGroupName string) error {
+	parameters := armresources.ResourceGroup{
+		Location: &aksSession.Region,
+	}
+
+	if _, err := aksSession.ResourceGroupClient.CreateOrUpdate(*ctx, resourceGroupName, parameters, nil); err != nil {
+		return fmt.Errorf("failed to create resource group: %w", err)
+	}
+
+	return nil
+}
+
+func (aksSession *AKSSession) CreateVirtualNetwork(ctx *context.Context, resourceGroupName, virtualNetworkName string) error {
+	// TODO : Take address prefixes as parameter
+	vnetParams := armnetwork.VirtualNetwork{
+		Location: &aksSession.Region,
+		Properties: &armnetwork.VirtualNetworkPropertiesFormat{
+			AddressSpace: &armnetwork.AddressSpace{
+				AddressPrefixes: []*string{
+					to.Ptr("10.0.0.0/12"),
+				},
+			},
+		},
+	}
+
+	if _, err := aksSession.VirtualNetworksClient.BeginCreateOrUpdate(*ctx, resourceGroupName, virtualNetworkName, vnetParams, nil); err != nil {
+		return fmt.Errorf("failed to create virtual network: %w", err)
+	}
+
+	return nil
+}
+
+func (aksSession *AKSSession) CreateSubnet(ctx *context.Context, resourceGroupName, virtualNetworkName, subnetName string) error {
+	// TODO : Take address prefix as parameter
+	subnetParams := armnetwork.Subnet{
+		Properties: &armnetwork.SubnetPropertiesFormat{
+			AddressPrefix: to.Ptr("10.8.0.0/16"),
+		},
+	}
+
+	if _, err := aksSession.SubnetsClient.BeginCreateOrUpdate(*ctx, resourceGroupName, virtualNetworkName, subnetName, subnetParams, nil); err != nil {
+		return fmt.Errorf("failed to create subnet: %w", err)
+	}
+
+	return nil
+}
+
+func (aksSession *AKSSession) CreateCluster(ctx *context.Context, kubernetesVersion, resourceGroupName, subnetID, vmSize string,
+	osType *armcontainerservice.OSType, osSKU *armcontainerservice.OSSKU, waitForClusterCreation bool) error {
+	// TODO : Take all IPs as params
+	clusterParams := armcontainerservice.ManagedCluster{
+		Location: &aksSession.Region,
+		Properties: &armcontainerservice.ManagedClusterProperties{
+			DNSPrefix:         &aksSession.ClusterName,
+			KubernetesVersion: &kubernetesVersion,
+			NetworkProfile: &armcontainerservice.NetworkProfile{
+				NetworkPlugin:    to.Ptr(armcontainerservice.NetworkPluginAzure),
+				DNSServiceIP:     to.Ptr("10.0.0.10"),
+				DockerBridgeCidr: to.Ptr("172.17.0.1/16"),
+				ServiceCidr:      to.Ptr("10.0.0.0/16"),
+			},
+			AddonProfiles: map[string]*armcontainerservice.ManagedClusterAddonProfile{
+				"httpApplicationRouting": {
+					Enabled: to.Ptr(true),
+				},
+			},
+			AgentPoolProfiles: []*armcontainerservice.ManagedClusterAgentPoolProfile{
+				{
+					Name:         to.Ptr("systempool1"),
+					Count:        to.Ptr(int32(5)),
+					VMSize:       &vmSize,
+					OSDiskSizeGB: to.Ptr(int32(100)),
+					OSSKU:        osSKU,
+					Mode:         to.Ptr(armcontainerservice.AgentPoolModeSystem),
+					OSType:       osType,
+					Type:         to.Ptr(armcontainerservice.AgentPoolTypeVirtualMachineScaleSets),
+					AvailabilityZones: []*string{
+						to.Ptr("1"),
+						to.Ptr("2"),
+						to.Ptr("3"),
+					},
+					VnetSubnetID: &subnetID,
+				},
+			},
+			ServicePrincipalProfile: &armcontainerservice.ManagedClusterServicePrincipalProfile{
+				ClientID: &aksSession.Cred.AKSCredentials.aksServicePrincipalID,
+				Secret:   &aksSession.Cred.AKSCredentials.aksServicePrincipalSecret,
+			},
+		},
+	}
+
+	poller, err := aksSession.AKSClient.BeginCreateOrUpdate(*ctx, resourceGroupName, aksSession.ClusterName, clusterParams, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create Kubernetes cluster: %w", err)
+	}
+
+	if !waitForClusterCreation {
+		return nil
+	}
+
+	if _, err := poller.PollUntilDone(*ctx, nil); err != nil {
+		return fmt.Errorf("failed to wait for Kubernetes cluster creation to complete: %w", err)
+	}
+
+	return nil
+}
+
+func (aksSession *AKSSession) CreateNodePool(ctx *context.Context, resourceGroupName, nodePoolName, subnetID, vmSize string,
+	count, diskSize int32, osType *armcontainerservice.OSType, osSKU *armcontainerservice.OSSKU, waitForNodeGroupCreation bool) error {
+	if err := ValidateOSType(osType); err != nil {
+		return fmt.Errorf("invalid os type: %w", err)
+	}
+
+	if *osType == armcontainerservice.OSTypeWindows && *osSKU != "" {
+		return fmt.Errorf("invalid os sku type for os type windows: %w", ErrInvalidOSSKUType)
+	}
+
+	if err := ValidateOSSKUType(osSKU); *osType != armcontainerservice.OSTypeWindows && err != nil {
+		return fmt.Errorf("invalid os sku type: %w", err)
+	}
+
+	nodePoolParams := armcontainerservice.AgentPool{
+		Properties: &armcontainerservice.ManagedClusterAgentPoolProfileProperties{
+			Count:        &count,
+			VMSize:       &vmSize,
+			OSDiskSizeGB: &diskSize,
+			OSSKU:        osSKU,
+			OSType:       osType,
+			Mode:         to.Ptr(armcontainerservice.AgentPoolModeUser),
+			Type:         to.Ptr(armcontainerservice.AgentPoolTypeVirtualMachineScaleSets),
+			AvailabilityZones: []*string{
+				to.Ptr("1"),
+				to.Ptr("2"),
+				to.Ptr("3"),
+			},
+			VnetSubnetID: &subnetID,
+		},
+	}
+
+	poller, err := aksSession.NodePoolClient.BeginCreateOrUpdate(*ctx, resourceGroupName, aksSession.ClusterName, nodePoolName, nodePoolParams, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create node pool: %w", err)
+	}
+
+	if !waitForNodeGroupCreation {
+		return nil
+	}
+
+	if _, err := poller.PollUntilDone(*ctx, nil); err != nil {
+		return fmt.Errorf("failed to wait for Kubernetes node pool creation to complete: %w", err)
+	}
+
+	return nil
+}
