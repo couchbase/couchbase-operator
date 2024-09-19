d3611f07f813a1fda957a22b00cd52ca2e583946
Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
2024-09-19 14:46:12 +0530

    add additional rebalance rest apis
    Change-Id: Icc687ec4e2ec64f18ae721773e75b5deca7d07a2
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/216392
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- test/cao_test_runner/util/cb_rest_api/cluster_nodes/rebalance.go
+++ test/cao_test_runner/util/cb_rest_api/cluster_nodes/rebalance.go
@@ -1,6 +1,9 @@
 package clusternodes
 
 import (
+	"net/url"
+	"strconv"
+
 	requestutils "github.com/couchbase/couchbase-operator/test/cao_test_runner/util/request"
 )
 
@@ -35,5 +38,96 @@ func RebalanceReport(hostname, reportID string) *requestutils.Request {
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
+	}
+}
+
+// RebalanceStop stops an ongoing rebalance.
+/*
+ * POST :: /controller/stopRebalance.
+ * CB Documentation not available.
+ * Success gives 200 OK. Failure to authenticate gives 401 Unauthorized. A malformed URI gives 404 Object Not Found.
+ */
+func RebalanceStop(hostname string) *requestutils.Request {
+	return &requestutils.Request{
+		Host:   hostname,
+		Path:   "/controller/stopRebalance",
+		Method: "POST",
+		Body:   nil,
+	}
+}
+
+type RebalanceRetryStruct struct {
+	Enabled         bool `json:"enabled"`
+	AfterTimePeriod int  `json:"afterTimePeriod"` // Time period in seconds. Values from 5 to 3600.
+	MaxAttempts     int  `json:"maxAttempts"`     // Values between 1 and 3.
+}
+
+// RebalanceRetry gets or sets the rebalance retry settings.
+/*
+ * GET / POST :: /settings/retryRebalance
+ * docs.couchbase.com/server/current/rest-api/rest-configure-rebalance-retry.html.
+ * Unmarshal into RebalanceRetryStruct for GET / POST.
+ * For POST request, the request body has x-www-form-urlencoded data based on RebalanceRetryStruct.
+ */
+func RebalanceRetry(hostname string, get bool, enabled bool, afterTimePeriod, maxAttempts int) *requestutils.Request {
+	// Performs GET request if enabled = true as we want to set the rebalance retry settings.
+	if get {
+		return &requestutils.Request{
+			Host:   hostname,
+			Path:   "/settings/retryRebalance",
+			Method: "GET",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+	}
+
+	// Else, POST request is made with the body having x-www-form-urlencoded data.
+	formData := url.Values{}
+
+	formData.Set("enabled", strconv.FormatBool(enabled))
+	formData.Set("afterTimePeriod", strconv.Itoa(afterTimePeriod))
+	formData.Set("maxAttempts", strconv.Itoa(maxAttempts))
+
+	return &requestutils.Request{
+		Host:   hostname,
+		Path:   "/settings/retryRebalance",
+		Method: "POST",
+		Headers: map[string]string{
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		Body: formData.Encode(),
+	}
+}
+
+// RebalancePendingRetry retrieves the pending rebalance retries.
+/*
+ * GET :: /pools/default/pendingRetryRebalance.
+ * docs.couchbase.com/server/current/rest-api/rest-get-rebalance-retry.html.
+ * Unmarshal into map[string]interface{}.
+ */
+func RebalancePendingRetry(hostname string) *requestutils.Request {
+	return &requestutils.Request{
+		Host:   hostname,
+		Path:   "/pools/default/pendingRetryRebalance",
+		Method: "GET",
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+}
+
+// RebalanceCancelRetry cancels the rebalance retries.
+/*
+ * POST :: /controller/cancelRebalanceRetry/<rebalance-id>.
+ * docs.couchbase.com/server/current/rest-api/rest-cancel-rebalance-retry.html.
+ * Prior to using this, retrieve the rebalanceID using RebalancePendingRetry().
+ * Success gives 200 OK. Failure to authenticate gives 401 Unauthorized. A malformed URI gives 404 Object Not Found.
+ */
+func RebalanceCancelRetry(hostname, rebalanceID string) *requestutils.Request {
+	return &requestutils.Request{
+		Host:   hostname,
+		Path:   "/controller/cancelRebalanceRetry/" + rebalanceID,
+		Method: "POST",
 	}
 }
