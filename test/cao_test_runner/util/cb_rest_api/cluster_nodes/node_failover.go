73876a96ca061c8bd49d3a437ac296f26ba5603d
Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
2024-10-14 15:36:40 +0530

    add cb node failover APIs
    Change-Id: I8185cd12b9801cef6636d78964fc9d5bd0469ad4
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/217875
    Tested-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>

---
+++ test/cao_test_runner/util/cb_rest_api/cluster_nodes/node_failover.go
@@ -0,0 +1,106 @@
+package clusternodes
+
+import (
+	"net/url"
+	"strconv"
+
+	requestutils "github.com/couchbase/couchbase-operator/test/cao_test_runner/util/request"
+)
+
+// PerformHardFailover initiates a hard failover for the specified OTP nodes.
+/*
+ * POST :: /controller/failOver.
+ * docs.couchbase.com/server/current/rest-api/rest-node-failover.html.
+ * NOTE: RebalanceStart to be performed after this with the failed over node in ejectedNodes and knownNodes both.
+ */
+func PerformHardFailover(hostname string, otpNodes []string, allowUnsafe bool) *requestutils.Request {
+	formData := url.Values{}
+
+	otpNodes = ConvertToOTPNames(otpNodes)
+	for _, otpNode := range otpNodes {
+		formData.Add("otpNode", otpNode)
+	}
+
+	if allowUnsafe {
+		formData.Set("allowUnsafe", strconv.FormatBool(allowUnsafe))
+	}
+
+	return &requestutils.Request{
+		Host:   hostname,
+		Path:   "/controller/failOver",
+		Method: "POST",
+		Headers: map[string]string{
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		Body: formData.Encode(),
+	}
+}
+
+// HardResetNode is used to reinitialize a node after an unsafe failover.
+/*
+ * POST :: /controller/hardResetNode.
+ * docs.couchbase.com/server/current/rest-api/rest-reinitialize-node.html.
+ * NOTE: All configuration settings and all data are lost.
+ * NOTE: Full Admin role is required.
+ */
+func HardResetNode(hostname string) *requestutils.Request {
+	return &requestutils.Request{
+		Host:   hostname,
+		Path:   "/controller/hardResetNode",
+		Method: "POST",
+		Headers: map[string]string{
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+	}
+}
+
+// PerformGracefulFailover initiates a graceful failover for the specified OTP nodes.
+/*
+ * POST :: /controller/startGracefulFailover.
+ * docs.couchbase.com/server/current/rest-api/rest-failover-graceful.html.
+ * NOTE: After this perform a rebalance, specifying all nodes in the cluster (including those gracefully failed over) as knownNodes.
+ * NOTE: And, additionally specifying those gracefully failed over as ejectedNodes.
+ */
+func PerformGracefulFailover(hostname string, otpNodes []string) *requestutils.Request {
+	formData := url.Values{}
+
+	otpNodes = ConvertToOTPNames(otpNodes)
+	for _, otpNode := range otpNodes {
+		formData.Add("otpNode", otpNode)
+	}
+
+	return &requestutils.Request{
+		Host:   hostname,
+		Path:   "/controller/startGracefulFailover",
+		Method: "POST",
+		Headers: map[string]string{
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		Body: formData.Encode(),
+	}
+}
+
+// SetFailoverRecoveryType sets the recovery type for the specified OTP node.
+/*
+ * POST :: /controller/setRecoveryType.
+ * docs.couchbase.com/server/current/rest-api/rest-node-recovery-incremental.html.
+ * NOTE: The recovery type for a node is set after the node is failed over and before rebalance.
+ */
+func SetFailoverRecoveryType(hostname, otpNode, recoveryType string) *requestutils.Request {
+	formData := url.Values{}
+
+	otpNodes := ConvertToOTPNames([]string{otpNode})
+
+	formData.Set("otpNode", otpNodes[0])
+	formData.Set("recoveryType", recoveryType)
+
+	return &requestutils.Request{
+		Host:   hostname,
+		Path:   "/controller/setRecoveryType",
+		Method: "POST",
+		Headers: map[string]string{
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		Body: formData.Encode(),
+	}
+}
