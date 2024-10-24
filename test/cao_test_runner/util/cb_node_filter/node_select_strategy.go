3bbe5fb263f536aee4e32cb65699f94ee459e9d5
Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
2024-10-24 20:55:33 +0530

    add cb_node_filter util
    Change-Id: I704af4d6349df94374b9f278501ea410dbff8bbc
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/218545
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>

---
+++ test/cao_test_runner/util/cb_node_filter/node_select_strategy.go
@@ -0,0 +1,47 @@
+package cbnodefilter
+
+import (
+	"fmt"
+	"math/rand"
+	"slices"
+)
+
+type CBNodeSelectStrategy string
+
+const (
+	// SelectSorted sorts the list of cb nodes based on ascending order of names.
+	SelectSorted CBNodeSelectStrategy = "sorted"
+
+	// SelectRandom randomizes the list of cb nodes.
+	SelectRandom CBNodeSelectStrategy = "random"
+)
+
+func SelectCBNodesUsingStrategy(strategy CBNodeSelectStrategy, count int, cbNodeNames []string) ([]string, error) {
+	if len(cbNodeNames) < count {
+		return nil, fmt.Errorf("select cb nodes using strategy `%s`: %w", strategy, ErrLessCBNodes)
+	}
+
+	switch strategy {
+	case SelectSorted:
+		return selectSortedCBNodes(count, cbNodeNames)
+	case SelectRandom:
+		return selectRandomCBNodes(count, cbNodeNames)
+	default:
+		return nil, fmt.Errorf("select cb nodes using strategy `%s`: %w", strategy, ErrSelectStrategyInvalid)
+	}
+}
+
+// selectSortedCBNodes sorts the cb nodes in ascending order of names and then selects required number of cb nodes.
+func selectSortedCBNodes(count int, cbNodeNames []string) ([]string, error) {
+	slices.Sort(cbNodeNames)
+	return cbNodeNames[0:count], nil
+}
+
+// selectRandomCBNodes randomizes the list of cb nodes and then selects required number of cb nodes.
+func selectRandomCBNodes(count int, cbNodeNames []string) ([]string, error) {
+	rand.Shuffle(len(cbNodeNames), func(i, j int) {
+		cbNodeNames[i], cbNodeNames[j] = cbNodeNames[j], cbNodeNames[i]
+	})
+
+	return cbNodeNames[0:count], nil
+}
