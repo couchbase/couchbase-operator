10d0b7da00a6afde8c320f11c30c6ffa9105c772
Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
2024-12-19 03:07:33 +0530

    update util retry function
    Change-Id: Ia67f64a8e857e5c1759fcf6fa316c38932998668
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/221161
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
    Reviewed-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>

--- test/cao_test_runner/util/util.go
+++ test/cao_test_runner/util/util.go
@@ -1,9 +1,12 @@
 package util
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -37,4 +40,51 @@ func RetryFunctionTillTimeout(functionToExec func() error, totalDuration time.Du
 	}
 
 	return fmt.Errorf("timeout reached without success: %w", err)
+}
+
+// RetryFunctionWithErrCatch retries a given function until the total duration is reached, with a specified interval between attempts.
+// If an error is returned which is present in the errList then execution is immediately returned.
+func RetryFunctionWithErrCatch(functionToExec func() error, totalDuration time.Duration, pollInterval time.Duration, errList []error) error {
+	if errList == nil {
+		errList = make([]error, 0)
+	}
+
+	startTime := time.Now()
+	ticker := time.NewTicker(pollInterval)
+	defer ticker.Stop()
+
+	var eg errgroup.Group
+
+	eg.Go(func() error {
+		var err error
+
+		for {
+			err = functionToExec()
+			if err == nil {
+				return nil
+			}
+
+			for _, targetErr := range errList {
+				if errors.Is(err, targetErr) {
+					return fmt.Errorf("retry function: encountered error to be caught: %w", err)
+				}
+			}
+
+			if time.Since(startTime) >= totalDuration {
+				return fmt.Errorf("retry function: timeout reached without success: %w", err)
+			}
+
+			select {
+			case <-ticker.C:
+				continue
+			}
+		}
+	})
+
+	err := eg.Wait()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
