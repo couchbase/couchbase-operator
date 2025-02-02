39047616b78ef6b3ca80d3dac81642921ce99885
Raghav S K <raghav.sk@couchbase.com>
2025-02-03 03:18:21 +0530

    Fixing run validator helper
    Added these changes to run the validators better
    1. Reconciles the validator config according to the tags mentioned before executing the validator
    2. An error during a validator run will immediately cause test failure and not wait for all validators to run.
    3. When yaml marshal etc fail, the validator run will error out causing test failure instead of just a log statement.
    
    Change-Id: Ia91e240f1934f4e8803f94efd0f8b39c7e54a2c3
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/222743
    Reviewed-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
    Tested-by: Raghav S K <raghav.sk@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>

--- test/cao_test_runner/validations/run_validation.go
+++ test/cao_test_runner/validations/run_validation.go
@@ -8,12 +8,12 @@ import (
 	"github.com/couchbase/couchbase-operator/test/cao_test_runner/actions/context"
 	"github.com/couchbase/couchbase-operator/test/cao_test_runner/assets"
 	"github.com/goccy/go-yaml"
-	"github.com/sirupsen/logrus"
 )
 
 var (
-	ErrUnableToMarshal       = errors.New("failed to marshal")
-	ErrFailedToMarshalConfig = errors.New("failed to marshal config object")
+	ErrUnableToMarshal            = errors.New("failed to marshal")
+	ErrFailedToMarshalConfig      = errors.New("failed to marshal config object")
+	ErrFailedToReconcileValidator = errors.New("failed to reconcile validator")
 )
 
 func checkValidValidation(validate string) Validator {
@@ -25,34 +25,27 @@ func checkValidValidation(validate string) Validator {
 }
 
 func RunValidator(ctx *context.Context, validators []map[string]any, state string, testAssets assets.TestAssetGetterSetter) (bool, error) {
-	var errs []error
-
 	for _, validatorMap := range validators {
 		if v := checkValidValidation(validatorMap["name"].(string)); v != nil {
 			var encoded []byte
 			encoded, err := yaml.Marshal(validatorMap)
-
 			if err != nil {
-				logrus.Error(fmt.Errorf("%w: %w", ErrFailedToMarshalConfig, err))
-				continue
+				return false, fmt.Errorf("run validator %w: %w", ErrFailedToMarshalConfig, err)
 			}
 
 			if err = yaml.NewDecoder(bytes.NewReader(encoded), yaml.Strict()).Decode(v); err != nil {
-				logrus.Error(fmt.Errorf("%w: %w", ErrUnableToMarshal, err))
-				continue
+				return false, fmt.Errorf("run validator %w: %w", ErrUnableToMarshal, err)
+			}
+
+			if _, err := ctx.ReconcileConfig(v); err != nil {
+				return false, fmt.Errorf("run validator %w: %w", ErrFailedToReconcileValidator, err)
 			}
 
 			if state == v.GetState() {
 				if err = v.Run(ctx, testAssets); err != nil {
-					errs = append(errs, err)
+					return false, fmt.Errorf("run validator: %w", err)
 				}
 			}
-		}
-	}
-
-	for _, err := range errs {
-		if err != nil {
-			return false, err
 		}
 	}
 
