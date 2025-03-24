1678419165b680a4f31139e7039b31205a8ea90e
Raghav S K <raghav.sk@couchbase.com>
2025-03-25 01:41:56 +0530

    CBQE-8431 : Adding a util for couchbase collection group resource
    Change-Id: Ie20cc7de960a1313045a45a32141bac26b8812a1
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/226625
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>
    Tested-by: Raghav S K <raghav.sk@couchbase.com>
    Tested-by: Aryaan Bhaskar <aryaan.bhaskar@couchbase.com>

---
+++ test/cao_test_runner/util/k8s/couchbase_resources/couchbase_collection_group_resource/types.go
@@ -0,0 +1,46 @@
+package couchbasecollectiongroupresource
+
+import "time"
+
+type Metadata struct {
+	Annotations       map[string]string   `json:"annotations"`
+	CreationTimestamp time.Time           `json:"creationTimestamp"`
+	Labels            map[string]string   `json:"labels"`
+	Name              string              `json:"name"`
+	Namespace         string              `json:"namespace"`
+	OwnerReferences   []MetadataOwnerRefs `json:"ownerReferences"`
+	ResourceVersion   string              `json:"resourceVersion"`
+	UID               string              `json:"uid"`
+}
+
+type MetadataOwnerRefs struct {
+	APIVersion         string `json:"apiVersion"`
+	BlockOwnerDeletion bool   `json:"blockOwnerDeletion"`
+	Controller         bool   `json:"controller"`
+	Kind               string `json:"kind"`
+	Name               string `json:"name"`
+	UID                string `json:"uid"`
+}
+
+type Spec struct {
+	History bool     `json:"history"`
+	MaxTTL  string   `json:"maxTTL"`
+	Names   []string `json:"names"`
+}
+
+type Status struct {
+}
+
+type CouchbaseCollectionGroupResource struct {
+	APIVersion string   `json:"apiVersion"`
+	Kind       string   `json:"kind"`
+	Metadata   Metadata `json:"metadata"`
+	Spec       Spec     `json:"spec"`
+	Status     Status   `json:"status"`
+}
+
+type CouchbaseCollectionGroupResourceList struct {
+	APIVersion                        string                              `json:"apiVersion"`
+	CouchbaseCollectionGroupResources []*CouchbaseCollectionGroupResource `json:"items"`
+	Kind                              string                              `json:"kind"`
+}
