b3d68731cd03e1c40b1983b27f48c4b84b343e89
BenMotts <ben.mottershead@couchbase.com>
2025-10-06 15:52:48 +0100

    K8S-4278: Skip last reconciled annotation when populating annotation warnings
    Change-Id: I3b633c800e496347179a4c915f62c906132b2b4a
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/234782
    Reviewed-by: Justin Ashworth <justin.ashworth@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- pkg/util/constants/constants.go
+++ pkg/util/constants/constants.go
@@ -129,7 +129,8 @@ const (
 	AnnotationDisableAdmissionController      = "dac.couchbase.com/skipDAC"
 	AnnotationSkipClusterNameLengthValidation = "dac.couchbase.com/skipClusterNameLengthValidation"
 	AnnotationForceDeleteLockfile             = "cao.couchbase.com/forceDeleteLockfile"
-	AnnotationLastReconciledSpec              = "cao.couchbase.com/lastReconciledSpec"
+
+	AnnotationLastReconciledSpec = "operator.couchbase.com/lastReconciledSpec"
 
 	ServerGroupLabel    = corev1.LabelTopologyZone
 	TopologyRegionLabel = corev1.LabelTopologyRegion
