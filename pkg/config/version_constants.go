a5e9d92eaa4ebf92ffb8124a7f4550000c1bb971
Aayush Senapati <aayushsenapati2002@gmail.com>
2025-11-12 14:09:39 +0530

    K8S-4338: Fix platform version is unsupported warning for K8S 1.33+ versions
    Change-Id: If5aadf2f01748a4ee06943f384cc4844a428a658
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/236431
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Yusuf Ramzan <yusuf.ramzan@couchbase.com>

--- pkg/config/version_constants.go
+++ pkg/config/version_constants.go
@@ -6,5 +6,5 @@ var (
 	// Note: these should be updated every release.
 	technicalLowerBound = &version.Info{Major: "1", Minor: "23", GitVersion: "v1.23.0"}
 	supportedLowerBound = &version.Info{Major: "1", Minor: "26", GitVersion: "v1.26.0"}
-	supportedUpperBound = &version.Info{Major: "1", Minor: "32", GitVersion: "v1.32.0"}
+	supportedUpperBound = &version.Info{Major: "1", Minor: "34", GitVersion: "v1.34.0"}
 )
