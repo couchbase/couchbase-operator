4a9f9a35ab1038a62b94e8d56c346319a7ff56fd
Yusuf Ramzan <yusuf.ramzan@couchbase.com>
2025-12-09 09:26:59 +0000

    fix eks
    Change-Id: Ie3154b66aa52f38b5f6897792ce006058601f9cd
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/237416
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ben Mottershead <ben.mottershead@couchbase.com>

---
+++ test/e2e/resources/provisioners/eks/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+}
