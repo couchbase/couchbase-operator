3158d1691073ad2cf2aee63e032b1f75e329233c
Blair Watt <blair.watt@couchbase.com>
2026-03-03 15:26:06 +0000

    CBD-6163: BSL License updates - Couchbase Operator Version 2.6
    Change-Id: I82dabdbc098265e49ce793ababe83896811181c4
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/241069
    Reviewed-by: Justin Ashworth <justin.ashworth@couchbase.com>
    Tested-by: Justin Ashworth <justin.ashworth@couchbase.com>

--- test/e2e/resources/build-cb-server.sh
+++ test/e2e/resources/build-cb-server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Copyright 2018-Present Couchbase, Inc.
+#
+# Use of this software is governed by the Business Source License included in
+# the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+# file, in accordance with the Business Source License, use of this software
+# will be governed by the Apache License, Version 2.0, included in the file
+# licenses/APL2.txt.
+
 VERSION=${1:-5.5.0}
 BUILD=${2:-2958}
 FLAVOR=${3:-vulcan}
