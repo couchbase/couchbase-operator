8d04c28bef90bf326d13dda6aa36d2990ae802d1
Blair Watt <blair.watt@couchbase.com>
2026-03-03 15:30:39 +0000

    CBD-6163: BSL License updates - Couchbase Operator Version 2.10
    Change-Id: I864077d9328fe582af2edc21da036631e85ab951
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/241073
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Justin Ashworth <justin.ashworth@couchbase.com>

--- pkg/util/couchbaseutil/api_test.go
+++ pkg/util/couchbaseutil/api_test.go
@@ -1,6 +1,16 @@
 //go:build apitest
 // +build apitest
 
+/*
+Copyright 2025-Present Couchbase, Inc.
+
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+*/
+
 package couchbaseutil
 
 import (
