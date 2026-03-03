42c6c24046ed5e8a8f193b2aae5f437059bf9ced
Blair Watt <blair.watt@couchbase.com>
2026-03-03 15:27:59 +0000

    CBD-6163: BSL License updates - Couchbase Operator Version 2.8
    Change-Id: I2e1774d42a39b8ec5350e916995ee762cccfd6d7
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/241071
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Justin Ashworth <justin.ashworth@couchbase.com>

--- pkg/util/eventschema/eventschema.go
+++ pkg/util/eventschema/eventschema.go
@@ -1,3 +1,13 @@
+/*
+Copyright 2018-Present Couchbase, Inc.
+
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+*/
+
 // Package eventschema allows flexible checking of k8s event streams via a schema like interface.
 //
 // Previous attempts to solve this problem checked whether two lists were equal.  There were a
--- scripts/deploy_debug.sh
+++ scripts/deploy_debug.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+# Copyright 2021-Present Couchbase, Inc.
+#
+# Use of this software is governed by the Business Source License included in
+# the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+# file, in accordance with the Business Source License, use of this software
+# will be governed by the Apache License, Version 2.0, included in the file
+# licenses/APL2.txt.
+
 CLUSTER_NAME=${CLUSTER_NAME:-couchbase-debug}
 
 API_SERVER_ADDRESS=${API_SERVER_ADDRESS-127.0.0.1}
