a5a4de7aebd32dcc4cf60358fa4939fb0f024f83
Blair Watt <blair.watt@couchbase.com>
2026-03-03 15:23:28 +0000

    CBD-6163: BSL License updates - Couchbase Operator Version 2.3
    Change-Id: I15a2ebb1465ee91e6cf61c6095f3da775651b484
    Reviewed-on: https://review.couchbase.org/c/couchbase-operator/+/241066
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Justin Ashworth <justin.ashworth@couchbase.com>

--- scripts/image_artifact_copy.sh
+++ scripts/image_artifact_copy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Copyright 2021-Present Couchbase, Inc.
+#
+# Use of this software is governed by the Business Source License included in
+# the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+# file, in accordance with the Business Source License, use of this software
+# will be governed by the Apache License, Version 2.0, included in the file
+# licenses/APL2.txt.
+
 # Parses the dockerignore files for explicit includes and copies them to a destination folder
 set -eu
 
